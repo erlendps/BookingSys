7b1ec8c74307e4b19101c858589972a19c6042cd
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-12-21 14:33:33 +0100

    add DB fields in class

--- bookingsys/core/src/main/java/com/github/erlendps/core/Group.java
+++ bookingsys/core/src/main/java/com/github/erlendps/core/Group.java
@@ -1,4 +1,15 @@
 package com.github.erlendps.core;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Embeddable
+@Entity
 public class Group {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  @ElementCollection
+  private Collection<User> members = new ArrayList<>();
 }
