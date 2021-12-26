087c68b686f790e09891826c4b4372deef5b3396
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-12-26 13:53:45 +0100

    finished the string building

--- bookingsys/src/main/java/com/github/erlendps/util/StringBuilding.java
+++ bookingsys/src/main/java/com/github/erlendps/util/StringBuilding.java
@@ -13,54 +13,100 @@ public class StringBuilding {
   }
 
   private static String buildStartString(User user, Group group) {
-    return "Hello " + user.getFirstName() + " there has been a change in group " +
-            group.getName() + "!\n";
+    return "Hello " + user.getFirstName() + ", there has been a change in group " +
+            group.getName() + "!\n\n";
   }
 
-  public static String singleDayStringNonBookersString(User nonBooker, SingleDayBooking booking) {
+  public static String singleDayNonBookersString(User nonBooker, SingleDayBooking booking) {
     StringBuilder sb = new StringBuilder();
     sb.append(buildStartString(nonBooker, booking.getGroup()));
     sb.append(booking.getBookable().getName()).append(" has been booked ");
     sb.append("on ").append(toWeekday(booking.getDate())).append(" ").append(booking.getDate());
     sb.append(" by ").append(booking.getUser().getFirstName()).append(".");
-    sb.append("\nView the changes here:\n");
-    sb.append("\nHave a nice day!");
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
     return sb.toString();
   }
 
-  public static String singleDayStringBookerString(SingleDayBooking booking) {
+  public static String singleDayBookerString(SingleDayBooking booking) {
     StringBuilder sb = new StringBuilder();
     sb.append("Hello ").append(booking.getUser().getFirstName()).append("!\n");
     sb.append("You have booked ").append(booking.getBookable().getName());
     sb.append(" on ").append(toWeekday(booking.getDate())).append(" ").append(booking.getDate());
     sb.append(" in group ").append(booking.getGroup().getName()).append(".");
-    sb.append("\nView the changes here:\n");
-    sb.append("\nHave a nice day!");
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
     return sb.toString();
   }
 
-  public static String multipleDayStringNonBookersString(User nonBooker,
-                                                        MultipleDayBooking booking) {
+  public static String multipleDayNonBookersString(User nonBooker, MultipleDayBooking booking) {
     StringBuilder sb = new StringBuilder();
     sb.append(buildStartString(nonBooker, booking.getGroup()));
     sb.append(booking.getBookable().getName()).append(" has been booked ");
     sb.append("from ").append(toWeekday(booking.getStartDate())).append(" ").append(booking.getStartDate());
     sb.append(" to ").append(toWeekday(booking.getEndDate())).append(" ").append(booking.getEndDate());
     sb.append(" by ").append(booking.getUser().getFirstName()).append(".");
-    sb.append("\nView the changes here:\n");
-    sb.append("\nHave a nice day!");
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
     return sb.toString();
   }
 
-  public static String multipleDayStringBookerString(MultipleDayBooking booking) {
+  public static String multipleDayBookerString(MultipleDayBooking booking) {
     StringBuilder sb = new StringBuilder();
     sb.append("Hello ").append(booking.getUser().getFirstName()).append("!\n");
     sb.append("You have booked ").append(booking.getBookable().getName());
     sb.append(" from ").append(toWeekday(booking.getStartDate())).append(" ").append(booking.getStartDate());
     sb.append(" to ").append(toWeekday(booking.getEndDate())).append(" ").append(booking.getEndDate());
     sb.append(" in group ").append(booking.getGroup().getName()).append(".");
-    sb.append("\nView the changes here:\n");
-    sb.append("\nHave a nice day!");
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
+    return sb.toString();
+  }
+
+  public static String removeSingleDayBookerString(SingleDayBooking booking) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello ").append(booking.getUser().getFirstName()).append("!\n");
+    sb.append("You have removed your booking on ").append(booking.getBookable().getName());
+    sb.append(" on ").append(toWeekday(booking.getStartDate())).append(" ").append(booking.getDate());
+    sb.append(" in group ").append(booking.getGroup().getName()).append(".");
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
+    return sb.toString();
+  }
+
+  public static String removeSingleDayNonBookersString(User nonBooker, SingleDayBooking booking) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(buildStartString(nonBooker, booking.getGroup()));
+    sb.append(booking.getUser().getFirstName()).append(" has removed the booking on ");
+    sb.append(booking.getBookable().getName());
+    sb.append(" on ").append(toWeekday(booking.getDate())).append(" ").append(booking.getDate());
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
+    return sb.toString();
+  }
+
+  public static String removeMultipleDayBookerString(MultipleDayBooking booking) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello ").append(booking.getUser().getFirstName()).append("!\n");
+    sb.append("You have removed your booking on ").append(booking.getBookable().getName());
+    sb.append(" from ").append(toWeekday(booking.getStartDate())).append(" ").append(booking.getStartDate());
+    sb.append(" to ").append(toWeekday(booking.getEndDate())).append(" ").append(booking.getEndDate());
+    sb.append(" in group ").append(booking.getGroup().getName()).append(".");
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
+    return sb.toString();
+  }
+
+  public static String removeMultipleDayNonBookersString(User nonBooker,
+                                                         MultipleDayBooking booking) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(buildStartString(nonBooker, booking.getGroup()));
+    sb.append(booking.getUser().getFirstName()).append(" has removed the booking on ");
+    sb.append(booking.getBookable().getName());
+    sb.append(" from ").append(toWeekday(booking.getStartDate())).append(" ").append(booking.getStartDate());
+    sb.append(" to ").append(toWeekday(booking.getEndDate())).append(" ").append(booking.getEndDate());
+    sb.append("\n\nView the changes here:\n");
+    sb.append("\n\nHave a nice day!");
     return sb.toString();
   }
 }
